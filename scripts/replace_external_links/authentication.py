f6ea6a51d2b48bc98478b4fc7c2c5f917e247d82
Eamon Ito-Fisher <eamon@itofisher.com>
2021-05-25 21:37:14 -0700

    Add HackMD authentication script

---
+++ scripts/replace_external_links/authentication.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+def authenticate(email, password, url):
+    session = requests.Session()
+    body = (f"email={requests.utils.quote(email)}&"
+            f"password={requests.utils.quote(password)}")
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        "User-Agent": "Python Script"
+    }
+
+    # Get CSRF token
+    res = session.get(url)
+    soup = BeautifulSoup(res.text, "lxml")
+    csrf_token = soup.select_one('meta[name="csrf-token"]')['content']
+    headers["X-XSRF-Token"] = csrf_token
+
+    res = session.post(url, data=body, headers=headers)
+    if res.status_code = 200:
+        return session
+    else:
+        return None
