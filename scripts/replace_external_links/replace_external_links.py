635c47f6881915281f723ff8650da9f23b66a56b
Eamon Ito-Fisher <eamon@itofisher.com>
2021-05-25 18:24:17 -0700

    Fix replace_external_links to preserve styling

--- scripts/replace_external_links/replace_external_links.py
+++ scripts/replace_external_links/replace_external_links.py
@@ -23,7 +23,7 @@ def get_image_links(md_file):
     links = list(filter(is_url, links))
     return links
 
-def make_image_path(md_file, link):
+def make_image_paths(md_file, link):
     md_dir, md_base = os.path.split(md_file)
     md_dir = md_dir.lstrip("./")
     # Remove '.md'
@@ -31,10 +31,13 @@ def make_image_path(md_file, link):
 
     name = link.split("/")[-1]
     # Remove any styling after image name
-    name = name.split(" ")[0]
+    name_parts = name.split(" ")
+    name = name_parts[0]
+    styling = " ".join(name_parts[1:])
 
     save_path = os.path.join(BASE_IMAGE_DIR, md_dir, md_base, name)
-    return save_path
+    rel_path = "/" + save_path + " " + styling
+    return save_path, rel_path
 
 def download_image(link, save_path):
     response = requests.get(link, stream=True)
@@ -65,18 +68,13 @@ def get_markdown_files(root):
                           if f.endswith(".md")])
     return all_files
 
-def save_2_rel(save_path):
-    # Ensure rel link starts with single '/'
-    return "/"  + save_path.lstrip("/")
-
 def update_markdown_file(md_file, replace_links):
     if not replace_links:
         return
     logger.info(f" Replacing links...")
     with open(md_file, 'r') as f:
         text = f.read()
-    for link, save_path in replace_links:
-        rel_link = save_2_rel(save_path)
+    for link, rel_link in replace_links:
         text = text.replace(link, rel_link)
         logger.info(f"  {link} -> {rel_link}")
     with open(md_file, 'w') as f:
@@ -92,10 +90,10 @@ def replace_links():
             logger.info(" Starting image download...")
             replace_links = []
             for link in image_links:
-                save_path = make_image_path(md_file, link)
+                save_path, rel_path = make_image_paths(md_file, link)
                 status_code = download_image(link, save_path)
                 if status_code == 200:
-                    replace_links.append((link, save_path))
+                    replace_links.append((link, rel_path))
             update_markdown_file(md_file, replace_links)
 
 if __name__ == "__main__":
