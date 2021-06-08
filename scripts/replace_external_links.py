923d4bcce14cdfe9cc81cf656e963f1c5a92d343
Eamon Ito-Fisher <eamon@itofisher.com>
2021-06-08 15:07:15 -0700

    Replace logger with core

--- scripts/replace_external_links.py
+++ scripts/replace_external_links.py
@@ -1,16 +1,13 @@
 import os
 import shutil
-import logging
+
+from actions_toolkit import core
 
 from util.markdown import get_image_links, update_markdown_file
 from util.links import get_files
 from util.authentication import authenticate_hackmd
 
 BASE_IMAGE_DIR = "assets"
-
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger("replace-external-links")
 
 
 def make_image_paths(md_file, link):
@@ -52,14 +49,14 @@ def download_image(session, link, save_path):
     """
     response = session.get(link, stream=True)
     if response.status_code == 200:
-        logger.info(f"  Saving {link} to {save_path}...")
+        core.info(f"  Saving {link} to {save_path}...")
         response.raw.decode_content = True
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
         with open(save_path, 'wb') as f:
             shutil.copyfileobj(response.raw, f)
     else:
-        logger.warning(f"  Failed to download {link}! "
-                       f"Error code {response.status_code}")
+        core.warning(f"  Failed to download {link}! "
+                     f"Error code {response.status_code}")
     return response.status_code
 
 
@@ -68,23 +65,23 @@ def replace_links():
     Iterates over markdown files in the root directory, downloading any
     external images and replacing the urls with relative links
     """
-    session = authenticate_hackmd(logger)
+    session = authenticate_hackmd()
 
-    logger.info("Starting image update process...")
-    md_files = get_files(".", logger, [".md"])
+    core.info("Starting image update process...")
+    md_files = get_files(".", [".md"])
     md_files = md_files[".md"]
     for md_file in md_files:
         image_links = get_image_links(md_file, filter_relative=False)
         if image_links:
-            logger.info(f"External links found in {md_file}!")
-            logger.info(" Starting image download...")
+            core.info(f"External links found in {md_file}!")
+            core.info(" Starting image download...")
             replace_links = []
             for link in image_links:
                 save_path, rel_path = make_image_paths(md_file, link)
                 status_code = download_image(session, link, save_path)
                 if status_code == 200:
                     replace_links.append((link, rel_path))
-            update_markdown_file(logger, md_file, replace_links)
+            update_markdown_file(md_file, replace_links)
 
 
 if __name__ == "__main__":
